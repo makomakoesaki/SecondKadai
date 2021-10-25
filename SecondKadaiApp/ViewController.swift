ee12f17a0c7274fb1283ec4d2d8e179a62392df0
makoto.esaki <it.net.makoto@icloud.com>
2021-10-25 19:31:28 +0900

    SecondKadaiAppを制作しました。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultVC:ResultViewController = segue.destination as! ResultViewController
+        resultVC.name = textField.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        }
 }
 
